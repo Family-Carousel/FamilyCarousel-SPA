50bfd09318838e1503e8f33f380020b0166f91e5
Michael Cavanaugh <funkel1989@gmail.com>
2019-10-13 21:31:51 -0400

    minor testing change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App deployed using aws amplify console"/>
   </div>
 </template>
 
