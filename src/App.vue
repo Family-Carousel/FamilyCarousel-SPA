a910b4d19d9283c8487440fbcf47a75414db819b
Michael Cavanaugh <funkel1989@gmail.com>
2019-10-13 22:45:08 -0400

    basic routing and some new pages

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app id="inspire">
+      <div>
+        <v-toolbar color="#0A3953" dark>
+          <v-toolbar-side-icon to="/"></v-toolbar-side-icon>
+          <v-toolbar-title>Family Carousel</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-toolbar-items class="hidden-sm-and-down"> 
+            <v-btn text to="/signup">Sign Up</v-btn>
+            <v-btn text to="/login">Login</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+      </div>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+
+      <v-footer class="pa-3" color="#0A3953" dark fixed>
+        <v-spacer></v-spacer>
+        <div>FamilyCarousel.com&nbsp; &copy; {{ new Date().getFullYear() }}</div>
+        <v-spacer></v-spacer>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script>
+
+export default {
+  name: "App",
+  components: {
+    
+  },
+  data: () => ({
+    //
+  })
+};
+</script>
