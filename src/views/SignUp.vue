e1f0c837f3ee7939d00091949b3a5eafd00a9cd9
Michael Cavanaugh <funkel1989@gmail.com>
2019-10-15 22:21:18 -0400

    adding custom dynamic layouts to the site

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <h1>This is an signup page</h1>
-  </div>
+  <v-flex xs12 sm8 md4>
+    <v-card class="elevation-12">
+      <v-toolbar dark color="primary">
+        <v-toolbar-title>Sign Up</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Login" name="login" prepend-icon="person" type="text"></v-text-field>
+          <v-text-field
+            id="password"
+            label="Password"
+            name="password"
+            prepend-icon="lock"
+            type="password"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary">Sign Up</v-btn>
+        <v-btn color="primary" to="/">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-export default {};
+import LoginOrSignUpLayout from "../layouts/LoginOrSignupLayout";
+
+export default {
+  created() {
+    this.$emit(`update:layout`, LoginOrSignUpLayout);
+  }
+};
 </script>
