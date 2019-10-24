616d35a659d02b40d1ddd0d4cd5b86fb6d1c7d6d
Michael Cavanaugh <funkel1989@gmail.com>
2019-10-23 23:40:35 -0400

    added signup validation

--- src/views/Auth/SignUp.vue
+++ src/views/Auth/SignUp.vue
@@ -13,13 +13,27 @@
       <v-card-text>
         <v-form>
           <v-text-field
+            @input="$v.displayName.$touch()"
+            @blur="$v.displayName.$touch()"
+            :error-messages="displayNameErrors"
+            required
             v-model="displayName"
             label="Display Name"
             name="username"
             prepend-icon="person"
             type="text"
           ></v-text-field>
-          <v-text-field v-model="email" label="Email" name="email" prepend-icon="email" type="text"></v-text-field>
+          <v-text-field
+            v-model="email"
+            label="Email"
+            name="email"
+            prepend-icon="email"
+            type="text"
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+            :error-messages="emailErrors"
+            required
+          ></v-text-field>
           <v-text-field
             v-model="password"
             id="password"
@@ -27,9 +41,16 @@
             name="password"
             prepend-icon="lock"
             type="password"
+            @input="$v.password.$touch()"
+            @blur="$v.password.$touch()"
+            :error-messages="passwordErrors"
+            required
           ></v-text-field>
           <v-checkbox
-            v-model="checkbox"
+            v-model="userAgreement"
+            @change="$v.userAgreement.$touch()"
+            @blur="$v.userAgreement.$touch()"
+            :error-messages="userAgreementErrors"
             :rules="[v => !!v || 'You must agree to continue']"
             required
             label="By signing up, I agree to Family Carousels terms of service and privacy policy"
@@ -54,6 +75,13 @@
 import LoginOrSignUpLayout from "../../layouts/LoginOrSignupLayout";
 import { Auth } from "aws-amplify";
 import { AmplifyEventBus } from "aws-amplify-vue";
+import { required, minLength, email } from "vuelidate/lib/validators";
+import {
+  hasNumber,
+  hasLowerCaseLetter,
+  hasUpperCaseLetter,
+  hasSpecialCharacter
+} from "../../validators/password";
 
 export default {
   created() {
@@ -74,8 +102,63 @@ export default {
       displayName: "",
       email: "",
       password: "",
-      checkbox: false
+      userAgreement: false
     };
+  },
+  validations: {
+    displayName: { required },
+    email: { required, email },
+    password: {
+      required,
+      minLength: minLength(8),
+      hasNumber,
+      hasLowerCaseLetter,
+      hasUpperCaseLetter,
+      hasSpecialCharacter
+    },
+    userAgreement: {
+      checked(val) {
+        return val;
+      }
+    }
+  },
+  computed: {
+    displayNameErrors() {
+      const errors = [];
+      if (!this.$v.displayName.$dirty) return errors;
+      !this.$v.displayName.required && errors.push("Username is required");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.required && errors.push("Password is required");
+      !this.$v.password.minLength &&
+        errors.push("Password must be minimum of 8 characters");
+      !this.$v.password.hasNumber &&
+        errors.push("Password must contain a number");
+      !this.$v.password.hasLowerCaseLetter &&
+        errors.push("Password must contain a lower case letter");
+      !this.$v.password.hasUpperCaseLetter &&
+        errors.push("Password must contain a capital letter");
+      !this.$v.password.hasSpecialCharacter &&
+        errors.push("Password must contain a special character");
+      return errors;
+    },
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.required && errors.push("Email is required");
+      !this.$v.email.email && errors.push("You must enter a valid email");
+      return errors;
+    },
+    userAgreementErrors() {
+      const errors = [];
+      if (!this.$v.userAgreement.$dirty) return errors;
+      !this.$v.userAgreement.required &&
+        errors.push("You must agree to continue");
+      return errors;
+    }
   },
   methods: {
     async isUserSignedIn() {
@@ -87,23 +170,25 @@ export default {
       }
     },
     async createAccount() {
-      this.apiRequest = true;
-      Auth.signUp({
-        username: this.email,
-        password: this.password,
-        attributes: {
-          email: this.email,
-          name: this.displayName
-        },
-        validationData: []
-      })
-        .then(() => {
+      this.$v.$touch();
+      if (!this.$v.$dirty) {
+        this.apiRequest = true;
+        Auth.signUp({
+          username: this.email,
+          password: this.password,
+          attributes: {
+            email: this.email,
+            name: this.displayName
+          },
+          validationData: []
+        }).then(() => {
           this.apiRequest = false;
           this.$router.push({
             path: "/confirmSignup",
             query: { email: this.email }
           });
         });
+      }
     }
   }
 };
