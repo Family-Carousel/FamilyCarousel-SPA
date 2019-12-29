708c2716711f6c5db1529f3b4541cb0b658b9935
Michael Cavanaugh <funkel1989@gmail.com>
2019-12-28 23:41:14 -0500

    changes to form

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -9,39 +9,34 @@
       <v-col cols="12" md="4" sm="12">
         <v-card class="pa-2 card-style" outlined title>
           <h1>Welcome to Family Carousel</h1>
-          <h5>Coming Soon! Subscribe to learn all about it</h5>
+          <h5>Coming Soon! Subscribe to learn all about it.</h5>
           <div id="mc_embed_signup">
             <form
+              ref="form"
               action="https://familycarousel.us4.list-manage.com/subscribe/post?u=57428f46a48f61cfe6e374001&amp;id=97f31a4664"
               method="post"
               id="mc-embedded-subscribe-form"
               name="mc-embedded-subscribe-form"
-              class="validate"
               target="_blank"
-              novalidate
             >
               <div id="mc_embed_signup_scroll">
-                <h2>Subscribe</h2>
-                <div class="indicates-required">
-                  <span class="asterisk">*</span> indicates required
+                <div class="mc-field-group">
+                  <v-text-field
+                    required
+                    label="Email Address"
+                    v-model="email"
+                    name="EMAIL"
+                    id="mce-EMAIL"
+                  />
                 </div>
                 <div class="mc-field-group">
-                  <label for="mce-EMAIL">
-                    Email Address
-                    <span class="asterisk">*</span>
-                  </label>
-                  <input type="email" value name="EMAIL" class="required email" id="mce-EMAIL" />
-                </div>
-                <div class="mc-field-group">
-                  <label for="mce-FNAME">
-                    First Name
-                    <span class="asterisk">*</span>
-                  </label>
-                  <input type="text" value name="FNAME" class="required" id="mce-FNAME" />
-                </div>
-                <div id="mce-responses" class="clear">
-                  <div class="response" id="mce-error-response" style="display:none"></div>
-                  <div class="response" id="mce-success-response" style="display:none"></div>
+                  <v-text-field
+                    required
+                    label="First Name"
+                    v-model="firstName"
+                    name="FNAME"
+                    id="mce-FNAME"
+                  />
                 </div>
                 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                 <div style="position: absolute; left: -5000px;" aria-hidden="true">
@@ -53,13 +48,14 @@
                   />
                 </div>
                 <div class="clear">
-                  <input
+                  <v-btn
+                    color="success"
+                    block
                     type="submit"
-                    value="Subscribe"
                     name="subscribe"
                     id="mc-embedded-subscribe"
-                    class="button"
-                  />
+                    @click="submitForm"
+                  >Subscribe</v-btn>
                 </div>
               </div>
             </form>
@@ -69,6 +65,24 @@
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      firstName: null
+    };
+  },
+  methods: {
+    submitForm() {
+      this.$refs.form.submit()
+      this.email = null;
+      this.firstName = null;
+    }
+  }
+};
+</script>
 
 <style>
 .card-style {
