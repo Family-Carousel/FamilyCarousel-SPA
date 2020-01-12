b97ae6a5720e10c2d9aff970cb6e022f23eaaed9
Michael Cavanaugh <funkel1989@gmail.com>
2020-01-12 16:57:20 -0500

    fleshed out basic store for family and members

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import { IAuthState } from './modules/auth/store.auth';
 import { IFamilyState } from './modules/family/store-family';
+import { IMemberState } from './modules/member/store-member';
 import { ISnackbarState } from './modules/snackbar/store-snackbar';
 
 Vue.use(Vuex);
@@ -10,6 +11,7 @@ export interface IRootState {
   SnackBarState: ISnackbarState;
   FamilyState: IFamilyState;
   AuthState: IAuthState;
+  MemberState: IMemberState;
 }
 
 export default new Vuex.Store<IRootState>({});
